f96d61bbd12a5352e8c77bf9e342baccca4bad8e
Marc Nieper-Wißkirchen <marc.nieper@gmail.com>
2016-04-04 17:44:19 +0200

    Update tests.scm

--- tests.scm
+++ tests.scm
@@ -1,11 +1,11 @@
 (import (scheme base)
 	(rapid test)
 	(rename (rapid test test) (run-tests run-rapid-test-tests))
-	(rename (rapid box test) (run-tests run-rapid-box-tests)))
+	(rename (rapid boxes test) (run-tests run-rapid-boxes-tests)))
 
 (test-begin "Rapid Libraries")
 
 (run-rapid-test-tests)
-(run-rapid-box-tests)
+(run-rapid-boxes-tests)
 
 (test-end "Rapid Libraries")
